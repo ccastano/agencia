08349570e7dbf569828d54f84d71674923d15fab
ccastano <12982613+ccastano@users.noreply.github.com>
2023-10-05 11:08:22 -0500

    Add files via upload
    [Infraestructura]: Se ajustan librerias.

---
+++ agencia/src/main/java/com/consultorio/registro/service/ConsultaPersonaRest.java
@@ -0,0 +1,32 @@
+package com.consultorio.registro.service;
+
+import java.util.List;
+
+import com.consultorio.registro.module.Persona;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+//http://localhost:8080/agencia/service/ConsultaPersonaRest/consultarPersona
+@Path("/ConsultaPersonaRest")
+@Stateless
+public class ConsultaPersonaRest {
+
+	EntityManager em;
+	
+	@GET
+	@Path("/consultarPersona")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Persona> findAll() {
+	    return em.createQuery("SELECT * FROM Persona p ORDER BY p.idPersona", Persona.class)
+	            .getResultList();
+	}
+	
+//    <datasource enabled="true" jndi-name="java:/jdbc/personas" jta="true" pool-name="personas" use-ccm="false" statistics-enabled="false">
+//    <connection-url>${per.url}</connection-url>
+	
+}
